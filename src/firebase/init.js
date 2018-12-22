b1c683e868f51094225c485755f1d5ec01d38d96
fakorede <abiolafakorede@gmail.com>
2018-12-22 22:37:58 +0100

    initialized firebase in project

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+import firestore from "firebase/firestore";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDmQXq2TL9jlw628jztHUtM0H0L0loWMpA",
+  authDomain: "vue-recipes-app.firebaseapp.com",
+  databaseURL: "https://vue-recipes-app.firebaseio.com",
+  projectId: "vue-recipes-app",
+  storageBucket: "vue-recipes-app.appspot.com",
+  messagingSenderId: "1077740582760"
+};
+const firebaseApp = firebase.initializeApp(config);
+
+// Export firestore database
+export default firebaseApp.firestore();
