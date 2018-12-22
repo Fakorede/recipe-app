437730b04edd16c8a67c89b46534ae0c232b7960
fakorede <abiolafakorede@gmail.com>
2018-12-22 22:11:40 +0100

    added icon and fuction for deleting recipe

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,6 +2,7 @@
   <div class="index container">
     <div class="card" v-for="recipe in recipes" :key="recipe.id">
       <div class="card-content">
+        <i class="material-icons delete" @click="deleteRecipe(recipe.id)">delete</i>
         <h2 class="indigo-text">{{ recipe.title }}</h2>
         <ul class="ingredients">
           <li v-for="(ingredient, index) in recipe.ingredients" :key="index">
@@ -47,6 +48,13 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    deleteRecipe(id) {
+      this.recipes = this.recipes.filter(recipe => {
+        return recipe.id != id;
+      });
+    }
   }
 };
 </script>
@@ -69,5 +77,13 @@ export default {
 }
 .index .ingredients li {
   display: inline-block;
+}
+.index .delete {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
 }
 </style>
