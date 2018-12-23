df7e54af17dbc3bf377492cd588e66aa2a7901c9
fakorede <abiolafakorede@gmail.com>
2018-12-23 21:57:05 +0100

    edit recipe component

---
+++ src/components/EditRecipe.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="edit-recipe container">
+        <h2>Edit Recipe {{ this.$route.params.recipe_slug }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "EditRecipe",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
